c0abd4d430aebe028390ea71bd5e5dd7d3023e5e
Aman Verma <amaverma@microsoft.com>
2021-12-07 18:25:33 +0530

    adding dockerfile

--- dataset-core/src/main/java/org/opengroup/osdu/dataset/swagger/HomeController.java
+++ dataset-core/src/main/java/org/opengroup/osdu/dataset/swagger/HomeController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping(value = "/swagger")
     public String swagger() {
-        System.out.println("swagger-ui.html");
-        return "redirect:swagger-ui.html";
+        System.out.println("swagger-ui/index.html");
+        return "redirect:swagger-ui/index.html";
     }
 }
